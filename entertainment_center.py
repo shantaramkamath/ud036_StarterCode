3610bce8e6223c5f128a668ea39b8c1823935506
Shantaram Kamath <shantaramkamath@gmail.com>
2019-04-15 15:03:31 +0530

    resubmission

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,44 @@
 import fresh_tomatoes
 from media import Movie
 
-#Creating different Movie objects
+# Creating different Movie objects
 pyaasa = Movie("Pyaasa",
-               "The film tells the story of Vijay, a struggling poet trying to make his works known in post-independence India",
-               "https://upload.wikimedia.org/wikipedia/en/9/9a/Pyaasa_1957_film_poster.jpg",
-               "https://www.youtube.com/watch?v=pBZYJAzz5ys")
+               "The film tells the story a struggling poet trying to make his works known in post-independence India",  # NOQA
+               "http://bit.ly/2UOpUeL",
+               "https://www.youtube.com/watch?v=pBZYJAzz5ys"  # NOQA
+               )
 
 three_idiots = Movie("3 idiots",
-                     "The film follows the friendship of three students at an Indian engineering college and is a satire about the social pressures under an Indian education system.",
-                     "https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg",
-                     "https://www.youtube.com/watch?v=xvszmNXdM4w")
+                     "The film is a satire about the social pressures under an Indian education system.",   # NOQA
+                     "http://bit.ly/2IyEgZZ",
+                     "https://www.youtube.com/watch?v=xvszmNXdM4w"  # NOQA
+                     )
 
 pk = Movie("pk",
            "The film questions religious dogmas and superstitions",
-           "https://upload.wikimedia.org/wikipedia/en/c/c3/PK_poster.jpg",
-           "https://www.youtube.com/watch?v=ZVmfNNVMcp0")
+           "http://bit.ly/2VHOI57",
+           "https://www.youtube.com/watch?v=ZVmfNNVMcp0"  # NOQA
+           )
 
 tzp = Movie("Taare Zameen Par",
-            "The film is about a 8 year old dyslexic child and his teacher who helps him to overcome his disability.",
-            "https://upload.wikimedia.org/wikipedia/en/b/b4/Taare_Zameen_Par_Like_Stars_on_Earth_poster.png",
-            "https://www.youtube.com/watch?v=tn_2Ie_jtX8")
+            "The film is about a 8 year old dyslexic child and his teacher who helps him to overcome his disability.",  # NOQA
+            "http://bit.ly/2X3VmD0",
+            "https://www.youtube.com/watch?v=tn_2Ie_jtX8"  # NOQA
+            )
 
 aaa = Movie("Andaaz Apna Apna",
-               "The film has achieved a cult classic status among Hindi audiences",
-               "https://upload.wikimedia.org/wikipedia/en/1/15/Andaz_Apna_Apna.jpg",
-               "https://www.youtube.com/watch?v=fd_w7Qw8biU")
+               "The film has achieved a cult classic status among Hindi audiences",   # NOQA
+               "http://bit.ly/2DfeOFk",
+               "https://www.youtube.com/watch?v=fd_w7Qw8biU"  # NOQA
+            )
 
 andhadhun = Movie("Andhadhun",
-                  "The film tells the story of a piano player who unwillingly becomes embroiled in the murder of a former film actor.",
-                  "https://upload.wikimedia.org/wikipedia/en/4/4a/Andhadhun_Movie_Poster.jpg",
-                  "https://www.youtube.com/watch?v=2iVYI99VGaw")
+                  "The film tells the story of a piano player who unwillingly becomes embroiled in the murder of a former film actor.",   # NOQA
+                  "http://bit.ly/2KwM1Ci",
+                  "https://www.youtube.com/watch?v=2iVYI99VGaw"   # NOQA
+                  )
 
-#creating list of movies
+# creating list of movies
 movies = [pyaasa, three_idiots, pk, tzp, aaa, andhadhun]
-#passing the list of movies as a parameter to fresh_tomatoes.open_movies_page() function
+# passing the list of movies as a parameter to fresh_tomatoes.open_movies_page() function     # NOQA
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -5,6 +5,3 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-
-        
